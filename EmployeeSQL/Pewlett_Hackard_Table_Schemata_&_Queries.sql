1478e95a6d68d2a22574d887d2c3137ce69c5c7e
symcd2020 <sym0002@uah.edu>
2024-02-07 19:31:35 -0600

    challenge updates

--- EmployeeSQL/Pewlett_Hackard_Table_Schemata_&_Queries.sql
+++ EmployeeSQL/Pewlett_Hackard_Table_Schemata_&_Queries.sql
@@ -1,7 +1,7 @@
 -- Create a new table
 CREATE TABLE departments (
   dept_no VARCHAR(30) PRIMARY KEY,
-  dept_name VARCHAR(255) NOT NULL 
+  dept_name TEXT NOT NULL 
 );
 
 -- Import data from CSV by using the "Import/Export Data..." option on the Table.
@@ -12,12 +12,12 @@ From departments
 
 -- Create a new table
 CREATE TABLE employees (
-  emp_no VARCHAR(30) PRIMARY KEY,
+  emp_no INT PRIMARY KEY,
   emp_title_id VARCHAR(30),
   birth_date DATE,
-  first_name VARCHAR(255),
-  last_name VARCHAR(255),
-  sex VARCHAR(30),
+  first_name TEXTT,
+  last_name TEXT,
+  sex TEXT,
   hire_date DATE
 );
 
@@ -31,7 +31,7 @@ From employees
 -- Create a new table
 CREATE TABLE dept_manager (
   dept_no VARCHAR(30),
-  emp_no VARCHAR(30),
+  emp_no INT,
   FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
   FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
@@ -45,7 +45,7 @@ From dept_manager
 
 -- Create a new table
 CREATE TABLE dept_emp2 (
-  emp_no VARCHAR(30) NOT NULL,
+  emp_no INT NOT NULL,
   dept_no VARCHAR(30) NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
   FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
@@ -60,7 +60,7 @@ From dept_emp2
 
 -- Create a new table
 CREATE TABLE salaries (
-  emp_no VARCHAR(30),
+  emp_no INT NOT NULL,
   salary INT,
   FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
@@ -75,7 +75,7 @@ From salaries
 -- Create a new table
 CREATE TABLE titles (
   title_id VARCHAR(30) PRIMARY KEY,
-  title VARCHAR(255)
+  title TEXT
 );
 
 -- Import data from CSV by using the "Import/Export Data..." option on the Table.
